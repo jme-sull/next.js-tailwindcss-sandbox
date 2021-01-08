0c3b0e0c6865aba99cbaa22ba27411bbeef988dc
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-01-08 18:14:54 -0500

    addds tailwind css and config

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,22 +1,23 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout from '../../components/layout'
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+import "tailwindcss/tailwind.css";
 
 export default function FirstPost() {
-
-    return (
+  return (
     <Layout>
-        <Head>
-            <title>First Post</title>
-        </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-           </h2> 
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+       
+      <button class="btn btn-green">
+                  Home
+      </button>
+  
+      </h2>
     </Layout>
-    )
-
+  );
 }
-
